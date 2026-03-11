d7485c1981e353a32a149f7c5d4bbaadee156f86
clarasb <clara.backens@brockmann-consult.de>
2026-03-11 15:06:58 +0100

    add `vitest-canvas-mock` dependency

--- chartlets.js/packages/lib/vitest.setup.ts
+++ chartlets.js/packages/lib/vitest.setup.ts
@@ -5,11 +5,4 @@
  */
 
 import "@testing-library/jest-dom";
-import { vi } from "vitest";
-import { createCanvas } from "canvas";
-
-// Mock getContext for HTMLCanvasElement
-HTMLCanvasElement.prototype.getContext = vi.fn().mockImplementation((type) => {
-  const canvas = createCanvas(100, 100);
-  return canvas.getContext(type);
-});
+import "vitest-canvas-mock";
